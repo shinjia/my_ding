b5d432bfeb992616da161ec8cd2ddec2fbe1b6f0
Shinjia Chen <shinjia168@gmail.com>
2024-04-16 00:16:36 +0800

    update

--- test/test2.php
+++ test/test2.php
@@ -24,10 +24,13 @@ $data = <<< HEREDOC
         <th>名稱</th>
         <th>單價</th>
         <th>備註</th>
-        <th>勾選</th>
+        <th>操作</th>
         <th>數量</th>
+        <th>附註</th>
     </tr>
 HEREDOC;
+
+$i = 0;
 foreach($a_menu as $value) {
     $a_item = explode(',', $value);
     array_map('trim', $a_item);
@@ -42,15 +45,28 @@ foreach($a_menu as $value) {
         <td>{$f_name}</td>
         <td>{$f_price}</td>
         <td>{$f_note}</td>
-        <td><input type="checkbox" name="want[]" value="{$f_code}"></td>
-        <td><input type="text" size="2" name="amt[{$f_code}]"></td>
+        <td>
+            <button onclick="add('amt_{$i}', 1);">加</a>
+            <button onclick="add('amt_{$i}', -1);">減</a>
+            
+        </td>
+        <td>
+            <input type="hidden" id="price_{$i}" value="{$f_price}">
+            <input type="text" name="amt[{$f_code}]" id="amt_{$i}" value="0" size="1" >
+        </td>
+        <td><input type="text" name="note[{$f_code}]" value="" size="4"></td>
     </tr>
 HEREDOC;
+    
+    $i++;  // 遞增 id 的索引值
 }
+
 $data .= <<< HEREDOC
 </table>
 <p>
-自行計算總金額：<input type="text" name="total_price" required>
+自動試算總金額：<input type="text" name="price_auto" id="price_auto" readonly value="">
+<br>
+確認輸入總金額：<input type="text" name="price_user">
 </p>
 <p>
 訂購人代號：<input type="text" name="membcode" required><br>
@@ -59,6 +75,7 @@ $data .= <<< HEREDOC
 <br><input type="submit" value="送出">
 </form>
 HEREDOC;
+
 
 $html = <<< HEREDOC
 <!DOCTYPE html>
@@ -76,6 +93,33 @@ $html = <<< HEREDOC
     <div>
     {$data}
     </div>
+
+<script>
+function add(_id, _cnt) {
+    let obj = document.getElementById(_id);
+    // 轉換當前值為數字並增加 _cnt
+    let new_value = parseInt(obj.value) + _cnt;
+    if(new_value<0) { new_value=0; }
+    obj.value = new_value;
+    calc_total();
+}
+
+function calc_total() {
+    let total = 0;
+    let i=0;
+    // 迴圈遍歷直到元素不存在為止
+    while(document.getElementById('price_'+i) && document.getElementById('amt_'+i)) {
+        // 獲取 price 和 amt 的值
+        var price = parseInt(document.getElementById('price_'+i).value);
+        var amt = parseInt(document.getElementById('amt_'+i).value);
+        // 計算乘積並累加到總額
+        total += price * amt;
+        // 移至下一對元素
+        i++;
+    }
+    document.getElementById('price_auto').value = total;
+}
+</script>
 </body>
 </html>
 HEREDOC;
--- test/test2_x.php
+++ test/test2_x.php
@@ -1,20 +1,30 @@
 <?php
 
-$total_price = $_POST['total_price'] ?? 0;
+// echo '<pre>';
+// print_r($_POST);
+// echo '</pre>';
+
+$price_user = $_POST['price_user'] ?? 0;
+$price_auto = $_POST['price_auto'] ?? 0;
 $membcode = $_POST['membcode'] ?? '';
 $membname = $_POST['membname'] ?? '無名氏';
-$a_want = $_POST['want'] ?? [];
 $a_amt = $_POST['amt'] ?? [];
+$a_note = $_POST['note'] ?? [];
 
 $data = '<ul>';
-foreach($a_want as $value) {
-    $data .= '<li>';
-    $data .= '項目：' . $value;
-    $data .= ' ===> 數量：' . $a_amt[$value];
-    $data .= '</li>';
+foreach($a_amt as $key=>$value) {
+    if($value!=0) {
+        $data .= '<li>';
+        $data .= '項目：' . $key;
+        $data .= ' ===> 數量：' . $value;
+        $data .= ' ===> 附註：' . $a_note[$key];
+        $data .= '</li>';
+    }
 }
 $data .= '</ul>';
-$data .= '總金額：' . $total_price;
+$data .= '總金額(自動計算)：' . $price_auto;
+$data .= '<br>';
+$data .= '總金額(用戶輸入)：' . $price_user;
 
 // 將此資料寫入指定的檔案內
 
@@ -29,7 +39,7 @@ $html = <<< HEREDOC
 <body>
     <h2>訂餐系統主要操作頁面測試</h2>
     <h3>顯示訂購人的資訊</h3>
-    <div>{$membcode} - {$membname}</div>
+    <div>[{$membcode}] {$membname}</div>
     <h3>顯示菜單的訂購資訊</h3>
     <div>
     {$data}
